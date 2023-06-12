211f8029a439c1ac0d5af9469141c882828061fd
ThànhCorn <99862284+ThanhCorn@users.noreply.github.com>
2023-06-12 10:59:40 +0700

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { FC, useState } from 'react'
-import './assets/styles/app.css'
-import './assets/styles/app.scss'
+import { FC } from 'react'
+import './styles/globals.css'
 
 const App: FC = () => {
-  const [fullname] = useState('Dư Thanh Được')
-  console.log(fullname)
   return (
     <div>
       <img src='' alt='React Logo' width={100} height={100} />
-      <h1>{fullname}</h1>
     </div>
   )
 }
